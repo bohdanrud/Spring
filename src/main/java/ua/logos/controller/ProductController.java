82800f058a956495dc916397ebb80f0d7d288d62
ащч <@SAMSUNG350U2B>
2018-04-10 17:19:46 +0300

    added saving to DB

--- src/main/java/ua/logos/controller/ProductController.java
+++ src/main/java/ua/logos/controller/ProductController.java
@@ -1,31 +1,44 @@
 package ua.logos.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ua.logos.entity.Product;
 import ua.logos.service.ProductService;
 
 @Controller
+@RequestMapping("/product")
 public class ProductController {
 
 	@Autowired
 	private ProductService productService;
 	
-	@GetMapping("/product/add-product")
+	@GetMapping("/add-product")
 	public String addProduct() {
 		return "product/add-product";
 	}
 	
-	@PostMapping("/product/add-product")
+	@PostMapping("/add-product")
 	public String saveProduct(
 			@RequestParam("productName") String productName,
 			@RequestParam("productDescription") String productDescription,
 			@RequestParam("productPrice") String productPrice
 			) {
 		System.out.println(">" + productName + " " + productDescription + " " + productPrice);
+		
+		Product product = new Product();
+		product.setName(productName);
+		product.setDescription(productDescription);
+		product.setPrice(new BigDecimal(productPrice + ".00"));
+		
+		productService.saveProduct(product);
+		
 		return "redirect:/";
 	}
 }
